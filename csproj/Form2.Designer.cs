a82d3feb73f55c6afede82520dfd2d8c10ec4ae7
Aeroshide <78432778+Aeroshide@users.noreply.github.com>
2021-02-05 00:53:02 +0700

    Add files via upload

---
+++ csproj/Form2.Designer.cs
@@ -0,0 +1,274 @@
+﻿
+namespace Project_idle_game
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.ok = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.agika1 = new System.Windows.Forms.Label();
+            this.agika = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.hhu = new System.Windows.Forms.Label();
+            this.uhh = new System.Windows.Forms.Label();
+            this.Drill = new System.Windows.Forms.Timer(this.components);
+            this.Autosave = new System.Windows.Forms.Timer(this.components);
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(347, 163);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 81);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "click";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // ok
+            // 
+            this.ok.AutoSize = true;
+            this.ok.Location = new System.Drawing.Point(704, 9);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(31, 15);
+            this.ok.TabIndex = 1;
+            this.ok.Text = "NaN";
+            this.ok.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.ok.Click += new System.EventHandler(this.ok_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(551, 78);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Buy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(551, 130);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Buy";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(548, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Mining device";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(554, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Vein mining";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(633, 85);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(32, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Qty :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(633, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Qty :";
+            // 
+            // agika1
+            // 
+            this.agika1.AutoSize = true;
+            this.agika1.Location = new System.Drawing.Point(671, 86);
+            this.agika1.Name = "agika1";
+            this.agika1.Size = new System.Drawing.Size(31, 15);
+            this.agika1.TabIndex = 8;
+            this.agika1.Text = "NaN";
+            this.agika1.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // agika
+            // 
+            this.agika.AutoSize = true;
+            this.agika.Location = new System.Drawing.Point(671, 131);
+            this.agika.Name = "agika";
+            this.agika.Size = new System.Drawing.Size(31, 15);
+            this.agika.TabIndex = 9;
+            this.agika.Text = "NaN";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(668, 8);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(39, 15);
+            this.label7.TabIndex = 10;
+            this.label7.Text = "Cash :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(704, 85);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(37, 15);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Cost :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(704, 131);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 15);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Cost :";
+            // 
+            // hhu
+            // 
+            this.hhu.AutoSize = true;
+            this.hhu.Location = new System.Drawing.Point(748, 86);
+            this.hhu.Name = "hhu";
+            this.hhu.Size = new System.Drawing.Size(31, 15);
+            this.hhu.TabIndex = 13;
+            this.hhu.Text = "NaN";
+            // 
+            // uhh
+            // 
+            this.uhh.AutoSize = true;
+            this.uhh.Location = new System.Drawing.Point(748, 131);
+            this.uhh.Name = "uhh";
+            this.uhh.Size = new System.Drawing.Size(31, 15);
+            this.uhh.TabIndex = 14;
+            this.uhh.Text = "NaN";
+            // 
+            // Drill
+            // 
+            this.Drill.Interval = 1000;
+            this.Drill.Tick += new System.EventHandler(this.Drill_Tick);
+            // 
+            // Autosave
+            // 
+            this.Autosave.Enabled = true;
+            this.Autosave.Interval = 60000;
+            this.Autosave.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(590, 358);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.uhh);
+            this.Controls.Add(this.hhu);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.agika);
+            this.Controls.Add(this.agika1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.ok);
+            this.Controls.Add(this.button1);
+            this.Name = "Form2";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Project Idle game";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label ok;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label agika1;
+        private System.Windows.Forms.Label agika;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label hhu;
+        private System.Windows.Forms.Label uhh;
+        private System.Windows.Forms.Timer Drill;
+        private System.Windows.Forms.Timer Autosave;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ csproj/Form2.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using Project_idle_game.Properties;
+
+namespace Project_idle_game
+{
+    public partial class Form2 : Form
+    {
+        int Cash = 0;
+        // Vein miner variables
+        int Vein = 0;
+        int VeinPrice = 50;
+        int VeinFunc = 1;
+        // Vein miner variables
+        // Mining device variables
+        int Dev = 0;
+        int DevPrice = 200;
+        int DevFunc = 5;
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            Cash += VeinFunc;
+            
+        }   
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ok.Text = Cash.ToString();
+            agika1.Text = Vein.ToString();
+            hhu.Text = VeinPrice.ToString();
+            agika.Text = Dev.ToString();
+            uhh.Text = DevPrice.ToString();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            if (Cash >= VeinPrice)
+            {
+                Cash -= VeinPrice;
+                Vein++;
+                VeinFunc++;
+                VeinPrice += 75;
+            }
+            else
+            {
+                MessageBox.Show("You dont have enough money!, Grind more to get money.");
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Cash >= DevPrice)
+            {
+                Cash -= DevPrice;
+                Dev++;
+                DevPrice += 100;
+                DevFunc += 5;
+                Drill.Start();
+            }
+            else
+            {
+                MessageBox.Show("You dont have enough money!, Grind more to get money.");
+            }
+        }
+
+        private void Drill_Tick(object sender, EventArgs e)
+        {
+            Cash += DevFunc;
+        }
+
+        private void ok_Click(object sender, EventArgs e)
+        {
+            Cash *= Cash;
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Cash = Properties.Settings.Default["cash"].GetHashCode();
+            MessageBox.Show("work!!");
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            
+            Settings.Default["Cash"] = Cash;
+            Properties.Settings.Default.Save();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Settings.Default["Cash"] = Cash;
+            Properties.Settings.Default.Save();
+
+        }
+    }
+}
